2af50bb0c22647b9a7224159bef1bc8fd6a23504
josh-dh <jdonellyhiggins@gmail.com>
2019-11-17 15:31:06 +0100

    built and tested first model

--- model.py
+++ model.py
@@ -14,6 +14,7 @@ from os import path
 
 #get pd dataframe for training data
 df_data = pd.read_csv("twitter-sentiment-analysis-hatred-speech/train.csv",names=('id','label','tweet'),header=None)
+tweetData = df_data.to_numpy().T[2]
 
 def processTweet(tweet):
 	tokens = tknzr.tokenize(tweet[0:-1])
@@ -27,10 +28,19 @@ def get_tweet_and_label(tweet_number):
 	return processTweet(tmp[2]), tmp[1]
 
 #train tf-idf vectorizer or get existing model
-tweetData = df_data.to_numpy().T[2]
 if (path.exists('vectorizer.joblib') == False):
 	vectorizer = TfidfVectorizer(tokenizer=processTweet, max_features=2000)
 	vectorizer.fit(tweetData)
 	dump(vectorizer, 'vectorizer.joblib')
 else:
 	vectorizer = load('vectorizer.joblib')
+
+#model
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+data = vectorizer.transform(tweetData)
+labels = df_data.to_numpy().T[1].astype(np.int_)
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size = 0.33)
+classifier = SVC()
+classifier.fit(X_train, y_train)
+print(classifier.score(X_test, y_test))
